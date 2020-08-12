7d879390c2614b046b60237eaf07d11c99e7f80e
krmanupa <60179455+krmanupa@users.noreply.github.com>
2020-08-12 23:46:47 +0530

    Create AutoRegisterLinuxHW.py

---
+++ AutoRegisterLinuxHW.py
@@ -0,0 +1,78 @@
+import subprocess
+import os
+from datetime import time
+from optparse import OptionParser
+
+def run_command(cmd):
+    proc = subprocess.Popen([cmd], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = proc.communicate()
+    return proc.returncode, stdout, stderr
+
+def download_omsagent(options):
+    workspaceId = options.workspace_id
+    workspaceKey = options.workspace_key
+
+    cmdToDownloadOmsAgent = "wget https://raw.githubusercontent.com/Microsoft/OMS-Agent-for-Linux/master/installer/scripts/onboard_agent.sh && sh onboard_agent.sh -w "+  str(workspaceId) +" -s "+ str(workspaceKey) +" -d opinsights.azure.com"
+
+    returncode, _, _ = run_command(cmdToDownloadOmsAgent)
+
+    if(returncode != 0):
+        return False
+    else:
+        cmdToInvokePerformedRequiredConfigChecks = "sudo su omsagent -c 'python /opt/microsoft/omsconfig/Scripts/PerformRequiredConfigurationChecks.py'"
+        run_command(cmdToInvokePerformedRequiredConfigChecks)
+        return True
+
+def register_woker(options):
+
+    workspaceId = options.workspace_id
+    automationSharedKey = options.automation_account_key
+    hybridGroupName = options.hybrid_worker_group_name
+    automationEndpoint = options.registration_endpoint
+
+    tries = 1
+    max_tries = 5
+    worker_present = False
+    while(not worker_present):
+        if tries > max_tries:
+            break
+        time_to_wait = 3 * (2 ** tries)
+        if time_to_wait > 60:
+            time_to_wait = 60
+        time.sleep(time_to_wait)
+        if(not os.path.isdir("/opt/microsoft/omsconfig/modules/nxOMSAutomationWorker/DSCResources/MSFT_nxOMSAutomationWorkerResource/automationworker")):
+            print("Worker isnt downloaded yet....")
+        tries += 1
+
+    if(worker_present):
+        cmdToRegisterUserHW = "sudo python /opt/microsoft/omsconfig/modules/nxOMSAutomationWorker/DSCResources/MSFT_nxOMSAutomationWorkerResource/automationworker/scripts/onboarding.py --register -w "+str(workspaceId)+" -k "+str(automationSharedKey)+" -g "+str(hybridGroupName)+" -e " +str(automationEndpoint)
+
+        run_command(cmdToRegisterUserHW)
+
+        cmdToTurnOffSignValidation = "sudo python /opt/microsoft/omsconfig/modules/nxOMSAutomationWorker/DSCResources/MSFT_nxOMSAutomationWorkerResource/automationworker/scripts/require_runbook_signature.py --false "+ str(workspaceId)
+
+        returncode, _, stderr = run_command(cmdToTurnOffSignValidation)
+        if(returncode != 0):
+            print("Registration failed because of "+str(stderr))
+            return
+        print("Successfully registerd worker.")
+    else:
+        print("Worker download failing...")
+
+
+def main():
+    parser = OptionParser(
+        usage="usage: %prog -e endpoint -k key -g groupname -w workspaceid -wk workspacekey")
+
+    parser.add_option("-e", "--endpoint", dest="registration_endpoint", help="Agent service registration endpoint.")
+    parser.add_option("-k", "--key", dest="automation_account_key", help="Automation account primary/secondary key.")
+    parser.add_option("-g", "--groupname", dest="hybrid_worker_group_name", help="Hybrid worker group name.")
+    parser.add_option("-w", "--workspaceid", dest="workspace_id", help="Workspace id.")
+    parser.add_option("-wk", "--workspacekey", dest="workspace_key", help="Workspace Key.")
+
+    (options, _) = parser.parse_args()
+    if(download_omsagent(options)):
+        register_woker(options)
+
+if __name__ == "__main__":
+    main()
