8bb07d9fac37803a325198fa865a9d5d0a2e9997
krmanupa <60179455+krmanupa@users.noreply.github.com>
2022-06-11 14:39:31 +0530

    Update Test.sh

--- VMExtensionScripts/Test.sh
+++ VMExtensionScripts/Test.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-if [! -z $(python3 -V 2>&1 | grep -Po '(?<=Python )(.+)') ]
+if [ "$#" -eq 0 ]
 then
-    #echo "python3 exists"; 
-    echo "python3"
-elif [! -z $(python2 -V 2>&1 | grep -Po '(?<=Python )(.+)') ]
-then
-    echo "python2"
-else 
-    echo "'python2' or 'python3' not found on this machine. Please install python."
-    exit 1
+	# Checking which python version is available 
+	# python3
+	if [[ -z $(python2 --version 2>&1 | grep 'not found') ]]
+	then
+		#echo "python exists"; 
+		pythonVer="python2"
+	elif [[ -z $(python3 --version 2>&1 | grep 'not found') ]]
+	then
+		#echo "python3 exists"; 
+		pythonVer="python3"
+	else 
+		echo "'python2' or 'python3' not found on this machine. Please install python."
+		exit 1
+	fi
+else
+	echo "parameters were passed."
 fi
+
