bb6657aa76196d70b5a0ad4839ac04a53aeefefc
krmanupa <60179455+krmanupa@users.noreply.github.com>
2022-06-11 14:17:14 +0530

    Update Test.sh

--- VMExtensionScripts/Test.sh
+++ VMExtensionScripts/Test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [! -z $(python3 -V 2>&1 | grep -Po '(?<=Python )(.+)') ]
-	then
-  #echo "python3 exists"; 
-  echo "python3"
+ then
+ #echo "python3 exists"; 
+ echo "python3"
 elif [! -z $(python2 -V 2>&1 | grep -Po '(?<=Python )(.+)') ]
-then
-  #echo "python2 exists"; 
-   echo "python2"
+ then
+ #echo "python2 exists"; 
+ echo "python2"
 else 
   echo "'python2' or 'python3' not found on this machine. Please install python."
   exit 1
